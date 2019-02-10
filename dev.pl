3384c98fe0187dc46dc087272f32d7840862fb93
Paul Hunter <paul.hunter@thomascook.com>
2019-02-10 19:03:49 +0000

    versioning test

--- dev.pl
+++ dev.pl
@@ -4,6 +4,7 @@
 # @Author Paul
 # @Created 10-Jan-2019 18:00:00
 #
+# @ GIT training file
 
 use 5.16.3;
 use strict;
--- devx.pl
+++ devx.pl
@@ -1,1 +1,11 @@
-#Perl file another one
+#!/usr/bin/perl
+#
+# @File updateFile.pl
+# @Author Paul
+# @Created 20-Oct-2018 22:39:01
+#
+# Secondary training
+
+use 5.16.3;
+use strict;
+use warnings;
